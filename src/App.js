32ed6ccbf8c89358593d34634acbc070fb062649
Tobias Ford <toby.ford@pobox.com>
2019-02-28 13:05:11 -0500

    converted 01-Login App.js to use reactstrap - first working version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
-import { Button } from 'reactstrap'
+import { Button, Navbar, NavbarBrand } from 'reactstrap'
 import './App.css'
 
 class App extends Component {
+  goTo (route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login () {
+    this.props.auth.login()
+  }
+
+  logout () {
+    this.props.auth.logout()
+  }
+
   render () {
+    const { isAuthenticated } = this.props.auth
+
     return (
       <div className='App'>
-        <header className='App-header'>
-          <Button color='primary'>Login</Button>
-        </header>
+        <Navbar>
+          <NavbarBrand href='/'>Auth0 Stub</NavbarBrand>
+          <Button color='primary' onClick={this.goTo.bind(this, 'home')}>Home</Button>
+          {
+            !isAuthenticated() && (
+              <Button color='primary' onClick={this.login.bind(this)} >Login</Button>
+            )
+          }
+          {
+            isAuthenticated() && (
+              <Button color='primary' onClick={this.logout.bind(this)} >Logout</Button>
+            )
+          }
+        </Navbar>
       </div>
     )
   }
