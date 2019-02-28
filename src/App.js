ce7a1389878d0906a29d6e18ef429bef46310462
Tobias Ford <toby.ford@pobox.com>
2019-02-28 12:00:54 -0500

    removed logo and added Login button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Button color="primary">Login</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
