253db9c0a0f0dcc6074f56d46472b46549019404
Tobias Ford <toby.ford@pobox.com>
2019-02-28 12:26:35 -0500

    added react-router-dom and the start of a routes file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App'
+import 'bootstrap/diet/css/bootstrap.min.css'
 import * as serviceWorker from './serviceWorker'
+import { createMainRoutes } from './routes'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const routes = createMainRoutes()
+
+ReactDOM.render(routes, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
